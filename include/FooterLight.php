316bd657a9354ad41b943b69a59f179a02415a04
piyapha24 <pm.piyaphat@gmail.com>
2024-09-16 16:09:26 +0700

    Update

---
+++ include/FooterLight.php
@@ -0,0 +1,18 @@
+<footer class="footer light">
+    <p>FLASHYOURMEME</p>
+
+    <ul class="social-list">
+        <li>
+            <a href="#" target="_blank"><img src="./assets/images/face-dark.png" width="24" alt=""></a>
+        </li>
+        <li>
+            <a href="#" target="_blank"><img src="./assets/images/ig-dark.png" width="24" alt=""></a>
+        </li>
+        <li>
+            <a href="#" target="_blank"><img src="./assets/images/line-dark.png" width="24" alt=""></a>
+        </li>
+    </ul>
+
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
